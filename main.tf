a1885d566ede56a0d384510b73d08bb49d90b7e9
jbeemster <joshua.a.beemster@gmail.com>
2025-10-28 09:00:34 +0100

    Add kernal filter for AMI lookup

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "amazon_linux_2023" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"]
+    values = ["al2023-ami-2023.*-kernel-6.12-x86_64"]
   }
 
   filter {
